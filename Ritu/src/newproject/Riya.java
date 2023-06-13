073b40f940b44e3163d5dde2ee5029f8766b9efb
co21r <co21r@DESKTOP-AE1NUI4>
2023-06-13 17:21:02 +0530

    Initial commit

---
+++ Ritu/src/newproject/Riya.java
@@ -0,0 +1,11 @@
+package newproject;
+
+public class Riya {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Riya jha");
+System.out.println("I am 18");
+	}
+
+}
---
+++ Ritu/src/newproject/Scoop.java
@@ -0,0 +1,12 @@
+package newproject;
+
+public class Scoop {
+	public static void main(String[] args) {
+		System.out.println(" This is another project");
+		
+	}
+		// TODO Auto-generated method stub
+
+	}
+
+
